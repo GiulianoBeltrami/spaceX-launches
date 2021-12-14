b34881448a6f6ffe74bfcf3809532ee8aec36f17
Giuliano Beltrami <juka2027@live.com>
2021-12-14 09:52:51 -0300

    Escolha e adição dos icones da página home

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Container, Row, Col, Nav } from 'react-bootstrap';
 import millify from 'millify';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import NoteIcon from '@mui/icons-material/Note';
+import LinkIcon from '@mui/icons-material/Link';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
 
 const { Link } = Nav;
 
 const Homepage = () => {
     return (
+        // Adicionar icones ao lado(esquerdo) de spacex, company ..
         <Container fluid>
             <Row className="pt-3">
                 <Col>
                     <h2>SpaceX project</h2>
-                    <p>
+                    <p class="lead">
                         This is an unofficial project, designed to show lauches from spaceX company along with some other company informations and news.
                         All the informations come from a public api.
                         <br />Source: <a className="text-dark" href="https://github.com/r-spacex/SpaceX-API">Api</a>
@@ -19,20 +25,26 @@ const Homepage = () => {
                     </p>
                 </Col>
             </Row>
-            <Row>
+
+            <Row className="align-middle">
                 <Col>
-                    <h2>Company informations</h2>
+                    <h2 className="d-inline">Company informations</h2>
+                    <RocketLaunchIcon className="home-icons-size pb-1" />
                 </Col>
             </Row>
+
             <Row>
                 <h2>Location</h2>
             </Row>
+
             <Row>
                 <h2>Summary</h2>
             </Row>
+
             <Row>
                 <h2>Links</h2>
             </Row>
+
             <Row>
                 <h2>News</h2>
             </Row>
